27029725c860790d1bf975302510dee16807bf07
Mueez Hajwani <mueezhajwani05@gmail.com>
2026-03-03 11:01:18 +0530

    test email

--- script.js
+++ script.js
@@ -73,14 +73,13 @@ window.addEventListener("scroll", () => {
 const contactForm = document.querySelector(".contact-form");
 if (contactForm) {
   contactForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-
     const name = this.querySelector('input[type="text"]')?.value;
     const email = this.querySelector('input[type="email"]')?.value;
     const subject = this.querySelector('input[placeholder="Subject"]')?.value;
     const message = this.querySelector("textarea")?.value;
 
     if (name && email && subject && message) {
+      e.preventDefault();
       alert("Thank you for your message! We will get back to you soon.");
       this.reset();
     } else {
